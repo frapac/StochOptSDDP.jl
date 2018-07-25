a2076e9bf0a801b90ade7f36898073dd75095338
Thomas Martin <mt.thomas.martin@gmail.com>
2018-07-25 17:02:12 +0200

    Added node creation in case matrixes are give, or a model. Added, function extracting cost and dynamics functions from matrixes Modified, Node creation when parameters are given.

--- src/node.jl
+++ src/node.jl
@@ -36,28 +36,13 @@ struct Solution <: SOI.AbstractSolution
     θf::Vector{Float64} # value of θ
 end
 
-function Node(timestep::Int64, cost::Function, dynamics::Function,
-              umin::Vector{Float64}, umax::Vector{Float64},
-              xmin::Vector{Float64}, xmax::Vector{Float64},
-              noises::IndependentProbability)
-    error("type of $(noises) is not supported")
-end
-
-function Node(timestep::Int64, cost::Function, dynamics::Function,
-              umin::Vector{Float64}, umax::Vector{Float64},
-              xmin::Vector{Float64}, xmax::Vector{Float64},
-              noises::IndependentProbability)
-    thisnode = NodeData()
-    thisnode = build_node!(node, timestep, cost, dynamics,umin, umax, xmin, xmax,
-                        noises)
-    return = thisnode
-end
-
 
 function build_dynamic_problem(timestep::Int64, cost::Function,
                 dynamics::Function, umin::Vector{Float64},
                 umax::Vector{Float64}, xmin::Vector{Float64},
-                xmax::Vector{Float64}, noises::IndependentProbability)
+                xmax::Vector{Float64}, noises::SOI.Probability,
+                cuts::Vector{AbstractCut})
+    Ncuts = length(cuts)
     Dimstate = size(xmin)
     Dimcontrol = size(umin)
     Dimalea = noises.laws.ndims
@@ -65,15 +50,15 @@ function build_dynamic_problem(timestep::Int64, cost::Function,
     w = Vector{Float64}(Dimalea)
     Modelpb = Model()
     @variable(Modelpb, xmin .<= x[1:Dimstate] .<= xmax)
-    @variable(Modelpb, alpha[1:Dimstate])
-    @variable(Modelpb, xf[1:Dimstate, 1:Dimalea])
-    @variable(Modelpb, u[1:Dimcontrol, 1:Dimalea])
+    @variable(Modelpb, xmin .<= xf[1:Dimstate] .<= xmax)
+    @variable(Modelpb, alpha)
+    @variable(Modelpb, umin .<= u[1:Dimcontrol] .<= umax)
 
-    # @objective(Modelpb, Min, cost(timestep, x, u, w))
-    @constraint(Modelpb, dynamics(timestep, x, u, w) == 0)
-    for i in [1:Dimalea]
-        @constraint(Modelpb, xmin .<=xf[:,i] .<= xmax)
-        @constraint(Modelpb, umin .<=u[:,i] .<= umax)
+    @objective(Modelpb, Min, cost(timestep, x, u, w) + alpha)
+
+    @constraint(Modelpb, dynamics(timestep, x, u, w) .== xf)
+    for k in [1:Ncuts]
+        @constraint(Modelpb, alpha >= dot(cuts[k].λ,xf) + cuts[k].β)
     end
     ##no constraint on alpha ????
     Nodepb = DynamicProgrammingModel(umin, umax, xmin, xmax, cost,
@@ -81,15 +66,154 @@ function build_dynamic_problem(timestep::Int64, cost::Function,
     return Nodepb
 end
 
+
+function create_cost(c::Array{Float64,1}, d::Array{Float64,1},
+                     f::Array{Float64,1})
+    function cost(t, x, u, w)
+        return dot(c, x) + dot(d, u) + dot(f, w)
+    end
+    return cost
+end
+
+
+function create_dynamics(A::Array{Float64,2}, B::Array{Float64,2},
+                        C::Array{Float64,2})
+    function dynamics(t, x, u, w)
+        return A*x + B*u + C*w
+    end
+    return dynamics
+end
+
+
+# cost : min{ cx+du+fw + alpha}
+# dynamics: Ax_{t} + Du_{t} + Ew_{t} = x_{t+1}
+#cuts:      alpha >= <lambda, x_{t+1}> + beta
+# bounds:   Fx_{t} <h
+#           Gu_{t} <j
+function build_dynamic_problem(timestep::Int64,
+                c::Array{Float64,1}, d::Array{Float64,1}, f::Array{Float64,1},
+                A::Array{Float64,2}, B::Array{Float64,2}, C::Array{Float64,2},
+                F::Array{Float64,1}, G::Array{Float64,1}, h::Array{Float64,1},
+                j::Array{Float64,1}, cuts::Vector{AbstractCut})
+    Ncuts = length(cuts)
+    Dimstate = length(c)
+    Dimcontrol = length(d)
+    Dimalea = length(f)
+
+    cost = create_cost(c, d, f)
+    dynamics = create_dynamics(A, B, C)
+    w=Vector{Float64}(Dimalea)
+
+    Modelpb = Model()
+    @variable(Modelpb, xmin .<= x[1:Dimstate] .<= xmax)
+    @variable(Modelpb, xmin .<= xf[1:Dimstate] .<= xmax)
+    @variable(Modelpb, alpha)
+    @variable(Modelpb, umin .<= u[1:Dimcontrol] .<= umax)
+    @objective(Modelpb, Min, cost(t, x, u, w) + alpha)
+    @constraint(Modelpb, dynamics(timestep, x, u, w) .== xf)
+    for k in [1:Ncuts]
+        @constraint(Modelpb, alpha >= dot(cuts[k].λ,xf) + cuts[k].β)
+    end
+    @contraint(Modelpb, F*x <= h)
+    @contraint(Modelpb, G*x <= j)
+    #extract bounds from h and j
+    Nodepb = DynamicProgrammingModel(-1*j[Dimcontrol+1:2*Dimcontrol],
+                                j[1:Dimcontrol], -1*h[Dimstate+1:2*Dimstate],
+                                h[1:Dimstate], cost, dynamics, Modelpb )
+    return Nodepb
+end
+
+# only fill in the pb field, the rest of the information is in the model
+function build_dynamic_problem(timestep, model)
+    Nodepb = DynamicProgrammingModel()
+    Nodepb.pb = model
+    return Nodepb
+end
+
+
 function build_node!(node ::NodeData, timestep::Int64, cost::Function,
                 dynamics::Function, umin::Vector{Float64},
                 umax::Vector{Float64}, xmin::Vector{Float64},
-                xmax::Vector{Float64}, noises::IndependentProbability)
-    dp = build_dynamic_problem(timestep, cost, dynamics, umin, umax, xmin, xmax, noises)
+                xmax::Vector{Float64}, noises::SOI.Probability,
+                cuts::Vector{AbstractCut})
+    dp = build_dynamic_problem(timestep, cost, dynamics, umin, umax,
+                                xmin, xmax, noises, cuts)
+    node.cuts = cuts
     node.timestep = timestep
     node.pb = dp
     node.noises = noises
 end
+
+
+#build node according a set of matrixes such that:
+# cost : min{ cx+du+fw + alpha}
+# dynamics: Ax_{t} + Du_{t} + Ew_{t} = x_{t+1}
+#cuts:      alpha >= <lambda, x_{t+1}> + beta
+# bounds:   Fx_{t} <h
+#           Gu_{t} <j
+function build_node!(node ::NodeData, timestep::Int64,c::Array{Float64,1},
+                d::Array{Float64,1}, f::Array{Float64,1}, A::Array{Float64,2},
+                B::Array{Float64,2}, C::Array{Float64,2}, F::Array{Float64,1},
+                G::Array{Float64,1}, h::Array{Float64,1}, j::Array{Float64,1},
+                noises::SOI.Probability, cuts::Vector{AbstractCut})
+
+    dp = build_dynamic_problem(timestep, c, d, f, A, B, C, F, G, h, j, cuts)
+    node.cuts = cuts
+    node.timestep = timestep
+    node.pb = dp
+    node.noises = noises
+end
+
+function build_node!(node ::NodeData, timestep::Int64, model::JuMP.Model,
+                    noises::SOI.Probability, cuts::Vector{AbstractCut})
+    dp = build_dynamic_problem(timestep, model)
+    node.cuts = cuts
+    node.timestep = timestep
+    node.pb = dp
+    node.noises = noises
+end
+
+function Node(timestep::Int64, cost::Function, dynamics::Function,
+              umin::Vector{Float64}, umax::Vector{Float64},
+              xmin::Vector{Float64}, xmax::Vector{Float64},
+              noises::SOI.Probability, cuts::Vector{AbstractCut})
+    if (isa(noises, IndependentProbability) ||isa(noises, MarkovProbability))
+        thisnode = NodeData()
+        build_node!(thisnode, timestep, cost, dynamics,umin, umax,
+                            xmin, xmax, noises, cuts)
+    else
+        error("noise type is invalid")
+    end
+    return = thisnode
+end
+
+
+function Node(timestep::Int64,c::Array{Float64,1},
+                d::Array{Float64,1}, f::Array{Float64,1}, A::Array{Float64,2},
+                B::Array{Float64,2}, C::Array{Float64,2}, F::Array{Float64,1},
+                G::Array{Float64,1}, h::Array{Float64,1}, j::Array{Float64,1},
+                noises::SOI.Probability, cuts::Vector{AbstractCut})
+    if (isa(noises, IndependentProbability) ||isa(noises, MarkovProbability))
+        thisnode = NodeData()
+        build_node!(thisnode, timestep, c, d, f, A, B, C, F, G, h, j,
+                                    noises, cuts)
+    else
+        error("noise type is invalid")
+    end
+    return = thisnode
+end
+
+function Node(timestep::Int64, model::JuMP.Model, noises::SOI.Probability,
+            cuts::Vector{AbstractCut})
+    if (isa(noises, IndependentProbability) ||isa(noises, MarkovProbability))
+        thisnode = NodeData()
+        build_node!(thisnode, timestep, model, noises, cuts)
+    else
+        error("noise type is invalid")
+    end
+    return = thisnode
+end
+
 
 # Solves the DynamicProgrammingModel of a node, provided xt and ξ.
 # Retruns a Solution
